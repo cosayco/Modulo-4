6c05225760615b91c7b735c8b4db3adb41735864
Carlos Duran <cosayco@gmail.com>
2024-09-19 16:12:07 -0300

    cambio a funciones flecha

--- sesion1/eje/assets/js/scripts.js
+++ sesion1/eje/assets/js/scripts.js
@@ -1,16 +1,20 @@
 function calcular() {
+    let monto = parseFloat(document.getElementById('monto').value);
+    let interes = parseFloat(document.getElementById('interes').value);
+    let plazo = parseInt(document.getElementById('plazo').value);
 
-    const monto = parseFloat(document.getElementById('monto').value);
-    const interes = parseFloat(document.getElementById('interes').value);
-    const plazo = parseInt(document.getElementById('plazo').value);
+    const factorInteres = (interes) => (1 + (interes / 100));
+    const montoPrestamo = (monto, interes) => (monto * interes);
+    const calculoCuota = (monto, plazo) => (monto / plazo);
+    const calculo = (cuota, plazo) => document.getElementById('resultado').innerHTML = `<h3>¡NO TE PREOCUPES! PUEDES PAGARLO EN ${plazo} CUOTAS DE $ ${cuota}.</h3>`;
 
     if (monto > 0 && interes >0 && plazo > 0){
-        let resultado = ((monto * (1 + (interes / 100))) / plazo)
-        document.getElementById('resultado').innerHTML = `<h3>¡NO TE PREOCUPES! PUEDES PAGARLO EN ${plazo} CUOTAS DE $ ${resultado}.</h3>`;
+        let valorInteres = factorInteres(interes);
+        let valorPrestamo = montoPrestamo(monto, valorInteres);
+        let valorCuota = calculoCuota(valorPrestamo, plazo);
+        calculo(valorCuota, plazo);
     } else {
-        document.getElementById('resultado').innerHTML = `<h3>¡DEBE INGRESAR VALORES PARA GENERAR EL CALCULO!.</h3>`;
-    }
-
-    
+        document.getElementById('resultado').innerHTML = `<h3>¡DEBE INGRESAR TODOS LOS VALORES PARA GENERAR EL CALCULO!.</h3>`;
+    }   
 }
   
