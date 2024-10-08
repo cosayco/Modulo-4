ae3082979f3177d09b5bd4e51c78588a4fea5e53
Carlos Duran <cosayco@gmail.com>
2024-10-07 22:54:07 -0300

    evaluacion final

--- final/assets/js/main.js
+++ final/assets/js/main.js
@@ -1,49 +1,66 @@
-/* $(document).ready(function () {
-    $("#principales").hover(function () {
+$(document).ready(function(){
 
-        $.ajax({
-            url: "https://swapi.dev/api/people/1",
-            method: "GET",
-            dataType: "json",
-            success: function(data){
-                let lista = ""
-                data.forEach(post => {
-                    lista += `<div>${post.height}</div>`
-                });
-                $("#principales").html(lista)
-            },
-                error: function (error) {
-                    console.log("Hubo un error", error.message)
-                }
+  function* cargarPersonajes(inicio, termino) {
+    let urlSW = "https://swapi.dev/api/people/"
+
+    for (let i = inicio; i <= termino; i++) {
+        let datos = yield $.ajax({
+          url: urlSW + i,
+          method: 'GET',
+          dataType: 'json'
         })
-    })
 
-})
- */
-
-/* $(document).ready(function(){
-    $('.block').hover(function() {
-      const block = $(this);
-      const url = block.data('url');
-      $.ajax({
-        url: url,
-        method: 'GET',
-        success: function(data) {
-          block.html(`<p>Nombre: ${data.name}</p><p>Estatura: ${data.height}</p><p>Peso: ${data.mass}</p>`);
-          block.show();
+        yield {
+          nombre: datos.name,
+          talla: datos.height,
+          peso: datos.mass
         }
-      });
-    });
-}); */
+    }
+  }
 
-document.querySelectorAll('.block').forEach(block => {
-    block.addEventListener('mouseenter', function() {
-      const url = this.getAttribute('data-url');
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          this.innerHTML = `<p>Nombre: ${data.name}</p><p>Estatura: ${data.height}</p><p>Peso: ${data.mass}</p>`;
-          this.style.display = 'block';
-        });
-    });
-});
+  function retraso(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  async function mostrarPersonajes(generator, id, color) {
+    detenerMostrar = false
+    for (let resultado = generator.next(); !resultado.done; resultado = generator.next(await resultado.value)){
+      if (detenerMostrar) break
+      if (resultado.value && resultado.value.nombre) {
+        $(id).append(`<div class="character"><p class="circulo ${color}"></p><strong>${resultado.value.nombre}</strong>Estatura: ${resultado.value.talla} cm. Peso: ${resultado.value.peso} kg.</div>`);
+        await retraso(1000);
+      }      
+    }
+  }
+
+  function limpiarPersonajes(){
+    $("#primeros").empty()
+    $("#segundos").empty()
+    $("#terceros").empty()
+  }
+
+  $("#primero").mouseenter(function() {
+    limpiarPersonajes()
+    let generator = cargarPersonajes(1, 5)
+    mostrarPersonajes(generator, "#primeros", "rojo")
+  }).mouseleave(function(){
+    detenerMostrar = true
+  })
+
+  $("#segundo").mouseenter(function() {
+    limpiarPersonajes()
+    let generator = cargarPersonajes(6, 10)
+    mostrarPersonajes(generator, "#segundos", "verde")
+  }).mouseleave(function(){
+    detenerMostrar = true
+  })
+
+  $("#tercero").mouseenter(function() {
+    limpiarPersonajes()
+    let generator = cargarPersonajes(12, 16)
+    mostrarPersonajes(generator, "#terceros", "azul")
+  }).mouseleave(function(){
+    detenerMostrar = true
+  })
+  
+})
